e137beed8ee63b2035b358a489b13d3f14ecfaa1
iker <iker.garay@ironchip.net>
2022-08-04 09:12:26 +0200

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+import IronchipCommonsIos
+
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+let rest = RestService()
+        rest.hello()
+    }
+}
