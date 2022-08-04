1c43da94f953c4d109febffcee142d5577eb76e6
iker <iker.garay@ironchip.net>
2022-08-04 13:40:20 +0200

    WIP

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -3,10 +3,10 @@ import CoreLocation
 
 public struct MyLibrary {
     var apiKey: String
-
-     public init(apikey: String) {
-          self.apiKey = apikey
-      }
+    
+    public init(apikey: String) {
+        self.apiKey = apikey
+    }
     
     public func transactionPost(transactionId: String, userId: String, extraData: [String:Any]) {
         let arch: String? = DeviceService().getArchitecture()
@@ -35,9 +35,9 @@ public struct MyLibrary {
         } catch{
             print(error.localizedDescription)
         }
-      
+        
         let connectedWifi: String! = getWifi()
-//        ModelsExtraDataRequest(from: <#Decoder#>)
+        //        ModelsExtraDataRequest(from: <#Decoder#>)
         let transaction = ModelsTransactionRequest(
             device: ModelsDeviceRequest(
                 id: uuid!,
@@ -47,30 +47,30 @@ public struct MyLibrary {
                     model: modelName!,
                     os: systemName!,
                     version: systemVersion!),
-                    rooted: rooted),
-             electromagneticLocation: nil,
-             extraData: jsonString,
-             geoLocation: ModelsGEOLocationRequest(
+                rooted: rooted),
+            electromagneticLocation: nil,
+            extraData: jsonString,
+            geoLocation: ModelsGEOLocationRequest(
                 latitude: lat,
                 longitude: long),
-                id: transactionId,
-                user: ModelsUserRequest(id: userId),
-                connected_bssid: connectedWifi) // ModelsTransactionRequest | Transaction info
+            id: transactionId,
+            user: ModelsUserRequest(id: userId),
+            connected_bssid: connectedWifi) // ModelsTransactionRequest | Transaction info
         print("JSONNNNNNN: ", String(describing: transaction))
-//        RestService().postRequest("https://testing.transaction.lbfraud.ironchip.com", data: transaction)
+        //        RestService().postRequest("https://testing.transaction.lbfraud.ironchip.com", data: transaction)
         let jsonData = Data(String(describing: transaction).utf8)
-
+        
         // create post request
         let url = URL(string: "https://testing.transaction.lbfraud.ironchip.com/transaction")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-
+        
         // insert json data to the request
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-           request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json;charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("Authorization", forHTTPHeaderField: apiKey)
         request.httpBody = jsonData
-
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
                 print(error?.localizedDescription ?? "No data")
@@ -81,19 +81,19 @@ public struct MyLibrary {
                 print(responseJSON)
             }
         }
-
+        
         task.resume()
         // transaction endpoint
-//        ClientAPI.transactionPost(apiKey: apiKey,transaction: transaction) { (response, error) in
-//            guard error == nil else {
-//                print(error ?? "[SWIFT LBFRAUD]: TransactionPost failed")
-//                return
-//            }
-//            if ((response) != nil) {
-//                print("[SWIFT LBFRAUD]: TransactionPost OK")
-//                dump(response)
-//            }
-//        }
+        //        ClientAPI.transactionPost(apiKey: apiKey,transaction: transaction) { (response, error) in
+        //            guard error == nil else {
+        //                print(error ?? "[SWIFT LBFRAUD]: TransactionPost failed")
+        //                return
+        //            }
+        //            if ((response) != nil) {
+        //                print("[SWIFT LBFRAUD]: TransactionPost OK")
+        //                dump(response)
+        //            }
+        //        }
     }
     
     func getWifi() -> String {
@@ -104,12 +104,12 @@ public struct MyLibrary {
             // Fallback on earlier versions
         }
         print("[WIFI]",wifi )
-         return wifi
-     }
+        return wifi
+    }
     
     func isDeviceJailbroken() -> Bool {
         return JailBroken().isDeviceJailbroken()
-       }
+    }
     
     func isCydiaAppInstalled() -> Bool {
         return JailBroken().isCydiaAppInstalled()
