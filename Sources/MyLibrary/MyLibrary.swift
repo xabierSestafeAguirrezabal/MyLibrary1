eff772c58686c7f34fea0a0c8a3d2a4c65546e64
iker <iker.garay@ironchip.net>
2022-08-05 13:30:46 +0200

    WIP

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -9,54 +9,37 @@ public struct MyLibrary {
     }
     
     public func transactionPost(transactionId: String, userId: String, extraData: [String:Any]) {
+        var extraDataSJON = ""
+
         let arch: String? = DeviceService().getArchitecture()
         let modelName: String? = DeviceService().modelName()
         let model: String? = DeviceService().model()
         let systemName: String? = DeviceService().systemName()
         let systemVersion: String? = DeviceService().systemVersion()
         var rooted: Bool? = false
+        
         let coord: CLLocationCoordinate2D = Gps().getCoordinates()
         let long = coord.longitude
-        print("[SWIFT LBFRAUD LONG]: ", long)
         let lat = coord.latitude
-        print("[SWIFT LBFRAUD LAT]: ", lat)
+        
         let uuid: String? = DeviceService().uuid()
-        let cydia: Bool? = isCydiaAppInstalled()
+        
+        let cydia: Bool? = JailBroken().isCydiaAppInstalled()
         if cydia == true {
             rooted = true
         } else {
-            rooted = isDeviceJailbroken()
+            rooted = JailBroken().isDeviceJailbroken()
         }
-        var jsonString: String = ""
+        
         do{
             let jsonData = try JSONSerialization.data(withJSONObject: extraData, options: [])
-            jsonString = String(data: jsonData, encoding: String.Encoding.ascii)!
+            extraDataSJON = String(data: jsonData, encoding: String.Encoding.ascii)!
             
         } catch{
             print(error.localizedDescription)
         }
         
-        let connectedWifi: String! = getWifi()
-        //        ModelsExtraDataRequest(from: <#Decoder#>)
-        let transaction = ModelsTransactionRequest(
-            device: ModelsDeviceRequest(
-                id: uuid!,
-                properties: ModelsDevicePropertiesRequest(
-                    cpuAbi: arch! as String,
-                    manufacturer: model!,
-                    model: modelName!,
-                    os: systemName!,
-                    version: systemVersion!),
-                rooted: rooted),
-            electromagneticLocation: nil,
-            extraData: jsonString,
-            geoLocation: ModelsGEOLocationRequest(
-                latitude: lat,
-                longitude: long),
-            id: transactionId,
-            user: ModelsUserRequest(id: userId),
-            connected_bssid: connectedWifi) // ModelsTransactionRequest | Transaction info
-        print("JSONNNNNNN: ", String(describing: transaction))
+        let connectedWifi: String! = WifiService().getWiFiBssid()
         
         let properties = [
             "cpu_abi": arch! as String,
@@ -77,19 +60,19 @@ public struct MyLibrary {
             "latitude": lat,
             "longitude": long,
         ]
-
+        
         let jsonObject = [
             "device": device,
             "electromagneticLocation": "",
-            "extraData": jsonString,
+            "extraData": extraDataSJON,
             "geoLocation": geoLocation,
             "id": transactionId,
             "user": user,
             "connected_bssid": connectedWifi!
         ] as [String : Any]
-        //        RestService().postRequest("https://testing.transaction.lbfraud.ironchip.com", data: transaction)
+        
         let jsonData = try? JSONSerialization.data(withJSONObject:jsonObject)
-
+        
         // create post request
         let url = URL(string: "https://testing.transaction.lbfraud.ironchip.com/transaction")!
         var request = URLRequest(url: url)
@@ -113,36 +96,6 @@ public struct MyLibrary {
         }
         
         task.resume()
-        // transaction endpoint
-        //        ClientAPI.transactionPost(apiKey: apiKey,transaction: transaction) { (response, error) in
-        //            guard error == nil else {
-        //                print(error ?? "[SWIFT LBFRAUD]: TransactionPost failed")
-        //                return
-        //            }
-        //            if ((response) != nil) {
-        //                print("[SWIFT LBFRAUD]: TransactionPost OK")
-        //                dump(response)
-        //            }
-        //        }
-    }
-    
-    func getWifi() -> String {
-        var wifi: String = ""
-        if #available(macCatalyst 14.0, *) {
-            wifi = WifiService().getWiFiBssid() ?? "error Wifi"
-        } else {
-            // Fallback on earlier versions
-        }
-        print("[WIFI]",wifi )
-        return wifi
-    }
-    
-    func isDeviceJailbroken() -> Bool {
-        return JailBroken().isDeviceJailbroken()
-    }
-    
-    func isCydiaAppInstalled() -> Bool {
-        return JailBroken().isCydiaAppInstalled()
     }
 }
 
