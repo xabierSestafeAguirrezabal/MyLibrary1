9630bfa6722c4d66232ef09059ea03cdb591a7fa
iker <iker.garay@ironchip.net>
2022-08-09 14:25:17 +0200

    WIP

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -21,30 +21,10 @@ public struct MyLibrary {
         }
         do {
             let decoded = try JSONDecoder().decode([String: String].self, from: result)
+            print(decoded.values)
         
         } catch {}
-              
-        let data = "[{\"form_id\":3465,\"canonical_name\":\"df_SAWERQ\",\"form_name\":\"Activity 4 with Images\",\"form_desc\":null}]".data(using: .utf8)!
-
-        struct Form: Codable {
-            let id: Int
-            let name: String
-            let description: String?
-
-            private enum CodingKeys: String, CodingKey {
-                case id = "form_id"
-                case name = "form_name"
-                case description = "form_desc"
-            }
-        }
-
-        do {
-            let f = try JSONDecoder().decode([Form].self, from: data)
-            print(f)
-            print(f[0])
-        } catch {
-            print(error)
-        }
+                                                                        
         //        let splitResult = result?.components(separatedBy: ":")
         //        let splitResult1    = splitResult?[1]
         //        let splitResult2 = splitResult1?.replacingOccurrences(of: "\"", with: "")
